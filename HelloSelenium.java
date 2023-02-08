4a5475f1a0d2c965f98398b51b6a24ef884a5c8f
karikalanonline <124452930+karikalanonline@users.noreply.github.com>
2023-02-08 11:51:24 +0530

    Add files via upload

---
+++ HelloSelenium.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class HelloSelenium {
+	public static void main(String[] args) {
+		System.out.println("Karikalan");
+		int inputOne = 2;
+		System.out.println(inputOne);
+		boolean inputTwo = true;
+		System.out.println(inputTwo);
+		String inputThree = "TestLeaf";
+		System.out.println(inputThree);
+		
+		
+	}
+
+}
