eb476098cac9a669431a2107839b52ec612ab8fc
karikalanonline <124452930+karikalanonline@users.noreply.github.com>
2023-02-08 11:53:43 +0530

    Add files via upload

---
+++ TwoWheeler.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class TwoWheeler {
+	public static void main(String[] args) {
+		int noOfWheels = 2;
+		System.out.println(noOfWheels);
+		short noOfMirrors = 2;
+		System.out.println(noOfMirrors);
+		long chasisNumber = 12344423456L;
+		System.out.println(chasisNumber);
+		boolean isPunctured = true;
+		System.out.println(isPunctured);
+		String bikeName = "Pulsar";
+		System.out.println(bikeName);
+		double runningKM = 5434.4356;
+		System.out.println(runningKM);	
+		
+	}
+
+}
