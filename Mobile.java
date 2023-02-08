73fbaf3f3fd77b57e5a043668ea8d94f0edc9efd
karikalanonline <124452930+karikalanonline@users.noreply.github.com>
2023-02-08 11:53:22 +0530

    Add files via upload

---
+++ Mobile.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class Mobile {
+	public static void main(String[] args) {
+		String mobileBrandName = "Redmi";
+		System.out.println(mobileBrandName);
+		char mobileLogo = 'M';
+		System.out.println(mobileLogo);
+		short noOfMobilePieces = 1;
+		System.out.println(noOfMobilePieces);
+		int modelNumber = 11;
+		System.out.println(modelNumber);
+		long mobileNumber = 123456678L;
+		System.out.println(mobileNumber);
+		float mobileprice = 12345.5678f;
+		System.out.println(mobileprice);
+		boolean isDamaged = false;
+		System.out.println(isDamaged);
+		
+		
+	}
+
+}
